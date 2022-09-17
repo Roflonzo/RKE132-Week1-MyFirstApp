55f7dfb604531ae6c03091473fea2033fc96f794
Roflonzo <113801500+Roflonzo@users.noreply.github.com>
2022-09-18 00:53:41 +0300

    Update Program.cs

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//app kysib kasutajalt tema nime
+//app tervitab teda nimepidi
+
+Console.WriteLine("Enter your name");
+//string - sõne
+string userName;
+
+userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!");
